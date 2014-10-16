0a098157d726b437bf804b61bfc77e929fa1abc8
Daniel Ku <kjunine@gmail.com>
2014-10-16 17:21:20 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'docker-pm2'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-pm2'
+long_description 'Installs/Configures docker-pm2'
+version          '0.1.0'
+
+depends 'docker'
